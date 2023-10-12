82769ad0f34cccf75dbb869cb9792d3d447f05b5
byepesg <byepesg@unal.edu.co>
2023-10-12 13:48:18 -0500

    Fixing paths table and Toolbar

--- src/layout/AppSidebar.vue
+++ src/layout/AppSidebar.vue
@@ -4,7 +4,7 @@ import AppMenu from './AppMenu.vue';
 
 <template>
     <app-menu></app-menu>
-    <img src="../../public/layout/images/logo-basil.png" height="150" class="mr-2" />
+    <img src="/layout/images/logo-basil.png" height="150" class="mr-2" />
 </template>
 
 <style lang="scss" scoped></style>
