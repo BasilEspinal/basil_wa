889c00dab2a6389afcb1a4f1732925bbcdfd2fde
byepesg <byepesg@unal.edu.co>
2024-01-05 21:04:49 -0500

    Changes for backup

--- src/layout/AppSidebar.vue
+++ src/layout/AppSidebar.vue
@@ -4,7 +4,7 @@ import AppMenu from './AppMenu.vue';
 
 <template>
     <app-menu></app-menu>
-    <sidebar-layout></sidebar-layout>
+    <!-- <sidebar-layout></sidebar-layout> -->
     <img src="/layout/images/logo-basil.png" height="150" class="mr-2" />
 </template>
 
