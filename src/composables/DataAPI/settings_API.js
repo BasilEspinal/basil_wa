e2fee7b25738e7e7408b0a4b377dbeb5ad1bd6d9
byepesg <byepesg@unal.edu.co>
2025-07-25 13:34:39 -0500

    Fix: URL updated after settings API was fixed

--- src/composables/DataAPI/settings_API.js
+++ src/composables/DataAPI/settings_API.js
@@ -12,35 +12,9 @@ export default function useSettingsAPI(locale) {
             'Accept-Language': locale //
         })
     };
-    
 
     return {
         APISettings,
         pathAPI
     };
 }
-
-// import { useI18n } from 'vue-i18n';
-// export default function useSettingsAPI() {
-//     const { locale } = useI18n();
-//     const APISettings = {
-//         token: '',
-//         baseURL: '',
-//         headers: new Headers({
-//             Accept: 'application/json',
-//             'Accept-Language': locale.value,
-//         })
-//     };
-//     function pathAPI() {
-//         //return {base:'http://agroonline_backend.test', apiVer:`/api/v1`}
-
-//          //return {base:'http://164.90.146.196:81', apiVer:`/api/v1`}
-//          return {base:'http://agroonline.co:81', apiVer:`/api/v1`}
-//     }
-//     return {
-//         APISettings,
-//         pathAPI
-//     };
-// }
-
-///useSettingsAPI.js
